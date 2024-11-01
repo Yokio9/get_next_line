e03315b7b3a4403feb75d094ea6ebf3303b1cf50
yokio9 <dmatayi@gmail.com>
2024-11-01 15:43:09 +0100

    (ノ ゜Д゜)ノ ︵ ┻━┻

---
+++ get_next_line_bonus.h
@@ -0,0 +1,24 @@
+#ifndef GET_NEXT_LINE_BONUS_H
+#define GET_NEXT_LINE_BONUS_H
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 1
+#endif
+
+#define MAX_FD 1024
+
+#include <stdlib.h>
+#include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*first_line(int fd, char *buffer);
+char	*next_line(char *buffer);
+char	*last_line(char *buffer);
+char	*fill_line(char *buffer, int i);
+int		ft_find_char(char *s, int c);
+void	*ft_calloc(size_t elem, size_t size);
+size_t	ft_strlen(const char *s);
+char	*gnl_strjoin(const char *s1, const char *s2);
+char	*free_me(char *str);
+
+#endif
